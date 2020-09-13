3f0dc8bda47253c5bfd7a1ebda7d7db9bf886ce8
miss xing <rxing@miu.edu>
2020-09-12 20:48:55 -0500

    First Servlet Project using web.xml as configuration.

---
+++ FirstServletDemo/src/main/java/miss/xing/HelloWorldServlet.java
@@ -0,0 +1,20 @@
+package miss.xing;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDateTime;
+
+public class HelloWorldServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/plain");
+        PrintWriter out = resp.getWriter();
+        out.println("Current Time: " + LocalDateTime.now());
+    }
+}
