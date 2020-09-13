065f4f75c297a0b2da30ba9e98d22a8488b2b30e
miss xing <rxing@miu.edu>
2020-09-13 17:13:36 -0500

    read properties file

--- simple-container/src/main/java/miss/xing/container/SimpleWebContainer.java
+++ simple-container/src/main/java/miss/xing/container/SimpleWebContainer.java
@@ -1,33 +1,46 @@
 package miss.xing.container;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.time.LocalDateTime;
+import java.util.Properties;
 
 public class SimpleWebContainer {
 
     private final int port;
+    private final String configFileName;
 
-    public SimpleWebContainer(int port) {
+    public SimpleWebContainer(int port, String configFileName) {
         this.port = port;
+        this.configFileName = configFileName;
     }
 
     private void start() throws IOException {
         ServerSocket serverSocket = new ServerSocket(port);
 
-        while(true) {
+        while (true) {
             Socket socket = serverSocket.accept();
             Thread socketHandler = new SocketHandler(socket);
             socketHandler.start();
         }
     }
 
+    private void loadPropertiesFile() throws IOException {
+        InputStream input = getClass().getClassLoader().getResourceAsStream(configFileName);
+        if (input == null) {
+            throw new RuntimeException("Unable to find file: " + configFileName);
+        }
+
+        Properties properties = new Properties();
+        properties.load(input);
+        properties.forEach((key, value) -> System.out.println(key + ", " + value));
+
+    }
+
     public static void main(String[] args) throws IOException {
-        SimpleWebContainer container = new SimpleWebContainer(8888);
+        SimpleWebContainer container = new SimpleWebContainer(8888, "config.properties");
+        container.loadPropertiesFile();
         container.start();
     }
 }
