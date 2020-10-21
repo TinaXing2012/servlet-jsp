6b6d4ab8b54ab078c6ec5d63da3791b269e7b30e
miss xing <rxing@miu.edu>
2020-10-20 21:43:35 -0500

    filter demo - DispatcherType

--- filter-demo/src/main/java/miss/xing/controller/WelcomeServlet.java
+++ filter-demo/src/main/java/miss/xing/controller/WelcomeServlet.java
@@ -15,7 +15,10 @@ public class WelcomeServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         System.out.println("WelcomeServlet doGet()...");
         System.out.println(req.getAttribute("handsomeguy"));
-        resp.setContentType("text/plain");
+//        resp.setContentType("text/plain");
+//        resp.getWriter().println("This is WelcomeServlet doGet()!!!");
+//        req.getRequestDispatcher("welcome.jsp").forward(req, resp);
+        req.getRequestDispatcher("welcome.jsp").include(req, resp);
         resp.getWriter().println("This is WelcomeServlet doGet()!!!");
     }
 }
---
+++ filter-demo/src/main/webapp/welcome.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: rujuanxing
+  Date: 10/20/20
+  Time: 9:23 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Welcome</title>
+</head>
+<body>
+<p>Request Attribute ${handsomeguy}</p>
+
+</body>
+</html>
